9db54edec32f4b1f167689662cebff0e87207258
taithedev2003@gmail.com <99863150+tai2319@users.noreply.github.com>
2024-10-26 09:25:58 +0700

    up

---
+++ tutorial/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Header</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New Blig</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
